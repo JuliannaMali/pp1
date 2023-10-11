87a19c5bcce69b0b7646524addc9be6c1887a6db
s-013-20 <s-013-20@gumiforest.ae.krakow.pl>
2023-10-11 12:39:30 +0200

    During class

---
+++ 02-TypesAndVariables/During-class/ex-10.py
@@ -0,0 +1,6 @@
+name = 'Julianna'
+age = 18
+height = 175
+years = age +6
+s = f'My name is {name}, I am {age} years old and my height is {height}. In 6 years I will be {years} year old.'
+print(s)
---
+++ 02-TypesAndVariables/During-class/ex-11.py
@@ -0,0 +1,7 @@
+Father = 3500
+mother= 4780
+number = 3
+income = Father + mother
+perperson = income/3
+s = f'Father earns: {Father}, Mother earns: {mother}, There is {number} people in family, the total income is {income}, Income per peron is {perperson}.'
+print(s)
