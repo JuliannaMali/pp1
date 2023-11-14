ea72c394d461866ac8ddc229e7676b0d239ce25c
V <mali.julianna6@gmail.com>
2023-11-14 21:49:13 +0100

    Before class

---
+++ 06-Arrays/Before-class/ex-7.py
@@ -0,0 +1,41 @@
+import random
+#a
+arr1=[3,7,1,0,4]
+#b
+arr2=[[2,3],[7,1],[0,4]]
+#c
+arr3=[7 for i in range(5)]
+#d
+arr4=[i for i in range(1,10)]
+#e
+arr5=[i*2 for i in range(1,10)]
+#f
+arr6=[random.randint(1,20) for i in range(10)]
+#g
+arr7=[[] for i in range(5)]
+#h
+arr8=[[1 for i in range(2)] for j in range(4)]
+#j
+arr9=[4,0,3]
+#k
+arr10=[0 for i in range(1,51)]
+#l
+arr11=[i for i in range(1,31)]
+#m
+arr12=[random.randint(1,2) for i in range(1,21)]
+#n
+arr13=[[False for i in range(1,3)] for j in range(1,6)]
+
+print(arr1)
+print(arr2)
+print(arr3)
+print(arr4)
+print(arr5)
+print(arr6)
+print(arr7)
+print(arr8)
+print(arr9)
+print(arr10)
+print(arr11)
+print(arr12)
+print(arr13)
